a2c1e20e1613442282884eb4b29a451818f5e6a5
Tomasz Kloda <tomasz.kloda@tum.de>
2021-03-30 01:12:42 +0000

    script to run the cortex benchmark suite updated to vary the number of ways

--- scripts_cachegrind/run_all_cortex.py
+++ scripts_cachegrind/run_all_cortex.py
@@ -8,13 +8,20 @@ import subprocess, shlex
 
 home_dir     = "/home/tkloda/cortexsuite/"
 valgrind_dir = "/home/tkloda/valgrind/bin/valgrind"
-results_dir  =  home_dir + "/cachegrind_results/" 
+results_dir  =  home_dir + "/cachegrind_results_per_way/" 
 
 benchmarks_list = []
 
 
-ways       = 4
-cache_line = 32
+#ways       = 4
+#cache_line = 32
+
+#ways       = 8
+# now we run each benchmark for 1,2,4,8,16, and 32 ways
+cache_line = 64
+
+
+benchmarks_list_large_ws = [ "disparity-fullhd", "disparity-vga", "kmeans-large", "liblinear-tlarge", "liblinear-tsmall", "pca-large", "pca-medium", "pca-small", "rbm-large", "rbm-medium", "sift-fullhd", "sift-vga", "spc-large", "spc-medium", "sphinx-large", "stitch-fullhd", "stitch-vga", "svd3-large", "tracking-vga", "tracking-fullhd"  ]
 
 benchmarks_list_cortex = [ "kmeans-small", "kmeans-medium", "kmeans-large", "spc-small", "spc-medium", "spc-large", "lda-small", "lda-medium", "lda-large", "liblinear-tlarge",  "liblinear-tmedium",  "liblinear-tsmall", "me-large",  "me-medium",  "me-small", "pca-large", "pca-medium", "pca-small", "rbm-large",  "rbm-medium",  "rbm-small", "sphinx-small", "sphinx-medium", "sphinx-large", "srr-large", "srr-medium",  "srr-small", "svd3-small", "svd3-medium", "svd3-large"  ]
 
@@ -160,7 +167,7 @@ def main():
     global benchmarks_list
     
     if len(sys.argv) != 3:
-        print "Specify [run|stats|get] [cortex|vision|all]"
+        print "Specify [run|stats|get] [cortex|vision|all|largews]"
         return 1
     
     if sys.argv[2] == "cortex":
@@ -169,6 +176,8 @@ def main():
         benchmarks_list = benchmarks_list_vision
     elif sys.argv[2] == "all":
         benchmarks_list = benchmarks_list_cortex + benchmarks_list_vision
+    elif sys.argv[2] == "largews":
+        benchmarks_list = benchmarks_list_large_ws
     else:
         print "Wrong scope [cortex|vision|all]"
         return 1
@@ -186,41 +195,51 @@ def main():
     return 0
 
 
+def valgrind_call(benchmark_name,cache_size, ways, line, policy, policy_opt=""):
 
-def valgrind_call(benchmark_name,cache_size, ways, line, policy, benchmark_cmd):
+    log_name = "results_" + benchmark_name + "_" + str(cache_size) + "_" + str(ways) + "ways_" + policy + str(policy_opt) + ".log"
+    benchmark_cmd = benchmarks_cmds[benchmark_name]
 
-    log_name = "results_" + benchmark_name + "_" + str(cache_size) + "_" + policy + ".log"
-
-    valgrind_cmd = "%s --tool=cachegrind --I1=%d,%d,%d --D1=%d,%d,%d --cache-policy=%s %s 2> %s 1> /dev/null &" % \
+    if policy=="bip":
+        #valgrind_cmd = "%s --tool=cachegrind --I1=%d,%d,%d --D1=%d,%d,%d --trace-children=yes --cache-policy=bip --bip-throttle=%s %s &> %s  &" % \
+        valgrind_cmd = "%s --tool=cachegrind --I1=%d,%d,%d --D1=%d,%d,%d --trace-children=yes --cache-policy=bip --bip-throttle=%s %s 2> %s 1> /dev/null &" % \
+            (valgrind_dir,cache_size,ways,line,cache_size,ways,line,policy_opt,benchmark_cmd,log_name)
+    else:
+        valgrind_cmd = "%s --tool=cachegrind --I1=%d,%d,%d --D1=%d,%d,%d --trace-children=yes --cache-policy=%s  %s 2> %s 1> /dev/null &" % \
             (valgrind_dir,cache_size,ways,line,cache_size,ways,line,policy,benchmark_cmd,log_name)
 
     os.system(valgrind_cmd)
 
 
 def run_all():
+    kB=1024
+    MB=1024*1024
     for benchmark in benchmarks_list:
         print benchmark
         os.chdir(benchmarks_dirs[benchmark])
         for cache_policy in ["lru", "lip", "random", "fifo"]:
-            for cache_size in [256,512,1024,2048,4096,8192,16384,32768,65536]:
-                valgrind_call(benchmark,cache_size,ways,cache_line,cache_policy,benchmarks_cmds[benchmark])
+            for cache_size in [1*kB,2*kB,4*kB,8*kB,16*kB,32*kB,64*kB,128*kB,256*kB,512*kB,1*MB,2*MB,4*MB,8*MB]:
+                for ways in [1,2,4,8,16,32]:
+                    valgrind_call(benchmark,cache_size,ways,cache_line,cache_policy)
+        for bip in [1.0/16, 1.0/32, 1.0/64]:
+            for cache_size in [1*kB,2*kB,4*kB,8*kB,16*kB,32*kB,64*kB,128*kB,256*kB,512*kB,1*MB,2*MB,4*MB,8*MB]:
+                for ways in [1,2,4,8,16,32]:
+                    valgrind_call(benchmark,cache_size,ways,cache_line,"bip",bip)
+
 
 def stats_all():
     for benchmark in benchmarks_list:
-       os.system("./get_stats.sh " + benchmarks_dirs[benchmark] + " " + benchmark + " &")
+       os.system("./get_more_stats.sh " + benchmarks_dirs[benchmark] + " " + benchmark + " &")
+
 
 def get_all():
     os.chdir(home_dir)
     os.system("mkdir -p " + results_dir )
     for benchmark in benchmarks_list:
-        stats_filename = benchmarks_dirs[benchmark] + "stats_" + benchmark + ".log"
-        copy_cmd       = "cp " + stats_filename + " " + results_dir
-        os.system(copy_cmd)
-
-    #find_and_copy_cmd = "find -name \"stats*\" -not -path \"" + "./" + results_dir + "/*\" " + " -exec cp \"{}\" " + results_dir  + " \; "
-    #os.system("mkdir -p " + results_dir )
-    #os.system(find_and_copy_cmd)
-
+        for  cache_policy in ["lru", "lip", "random", "fifo", "bip0.0625", "bip0.03125", "bip0.015625" ]:
+            stats_filename = benchmarks_dirs[benchmark] + "stats_" + benchmark + "_" + cache_policy + ".log"
+            copy_cmd       = "cp " + stats_filename + " " + results_dir
+            os.system(copy_cmd)
 
 
 
